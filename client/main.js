4e0ab4a8f9c24a2b93b2952e32f4ba11b68de736
Andres Atencio <and.atencio@gmail.com>
2015-04-10 08:46:49 -0300

    add superagent

--- client/main.js
+++ client/main.js
@@ -1,9 +1,14 @@
 var React = require('react');
-
-//var view = require('./views/view.jsx'); // need to specify the jsx extension
+var request = require('superagent');
 
 var hello = require('./views/landing.jsx');
 
-
-
 React.render(hello(), document.getElementById('content'));
+
+request
+	.post('/login')
+	.send(data)
+	.end(function(err, res){
+	   console.log(res)
+	   console.log(err)
+	});
