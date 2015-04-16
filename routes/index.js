845b65478639a2d2cac2dd5be74f3d47577334d4
Andres Atencio <and.atencio@gmail.com>
2015-04-16 10:48:23 -0300

    incluyendo navbar en html

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index');
-// });
-
-
 router.get('/auth', function (req, res) {
 	console.log(req.headers)
 	res.sendStatus(200)
